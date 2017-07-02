debb1c1c3dcdee896ef8f3c0d509c74ee79b251c
R Pranith Kumar <praneeth970@gmail.com>
2017-07-03 00:35:49 +0530

    Update upload_form.php

--- upload_form.php
+++ upload_form.php
@@ -27,24 +27,6 @@
 						<input type="text" placeholder="Enter project-ID" name="pid" required></br>
 					<label><b>Description</b></label>
 						<textarea placeholder="Give a discription" name="des" required></textarea></br>
-					<label><b>Extension</b></label>
-						<select name="ext">
-							<option value="txt">Text</option>
-							<option value="pdf">PDF</option>
-							<option value="docx">Docx</option>
-							<option value="c">C</option>
-							<option value="cpp">CPP</option>
-							<option value="java">Java</option>
-							<option value="py">Python</option>
-							<option value="html">HTML</option>
-							<option value="css">CSS</option>
-							<option value="js">JavaScript</option>
-							<option value="jpeg">Jpeg</option>
-							<option value="png">Png</option>
-							<option value="mp3">MP3</option>
-							<option value="mp4">MP4</option>
-							<option value="file">Other</option>
-						</select></br>
 					<label>File</label>
 						<input type="file" name="projfile" id="projfile"/>
 					<button type="submit">Upload here !</button>
